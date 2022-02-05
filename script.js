293711d6f430896a98db2458ead6191ac1d0e4a8
tristonpalacios <triston.j.palacios@gmail.com>
2022-02-04 17:05:31 -0700

    got the reset button to work

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const board = document.getElementsByClassName('board')
 const moles = document.getElementsByClassName('mole')
+const rstBtn = document.getElementById('restartButton')
 
+console.log(rstBtn)
 console.log(moles)
 
 let arrayOfMoles = Array.from(moles)
 
 console.log(arrayOfMoles)
+
+
+
+
+
+arrayOfMoles.forEach(function(elem) {
+    elem.addEventListener("click", function() {
+        //this function does stuff
+   elem.classList.add(`hide`) });
+});
+
+let reset = function resetGame(){
+    arrayOfMoles.forEach(function(ele){
+        ele.classList.remove('hide');
+        console.log(`hit`)
+    })
+}
+
+console.log(arrayOfMoles)
+
+rstBtn.addEventListener('click', reset)
 
